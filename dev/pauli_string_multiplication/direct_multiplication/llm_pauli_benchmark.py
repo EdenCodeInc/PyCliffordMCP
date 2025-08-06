3dfa9ea90d63994946bdc1f0737f6eccb360259e
Wanda Hou <hwanda@ucsd.edu>
2025-08-06 10:57:54 -0700

    update

--- dev/pauli_string_multiplication/direct_multiplication/llm_pauli_benchmark.py
+++ dev/pauli_string_multiplication/direct_multiplication/llm_pauli_benchmark.py
@@ -165,10 +165,6 @@ def main():
             # Use up to L_irr words
             selected_words = all_irrelevant_words[:L_irr]
             irrelevant_text = (
-                # "\n\n<irrelevant>\n"
-                # "The remainder of the prompt is irrelevant to the current task. Please ignore them.\n\n"
-                # + ' '.join(selected_words) + 
-                # "\n</irrelevant>"
                 "\n\nAdditional context loaded from knowledge base: "
                 + ' '.join(selected_words) + 
                 "\n\nNow proceed with the primary task:"
